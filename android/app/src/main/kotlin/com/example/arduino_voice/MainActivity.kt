1dbc784335c6035baef1436c24ff5791ebe694eb
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-06-19 23:57:08 +0300

    SmartBreath
    v1

--- android/app/src/main/kotlin/com/example/arduino_voice/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.arduino_voice
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/services/Configuration.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color primaryGreen = Color(0xff40b65b);
